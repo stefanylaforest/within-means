1efe978806cd97009083cca1da588e0191c6edc9
stefany laforest <stefanylaforest@live.ca>
2021-06-05 11:21:18 +0100

    add searchcontext to noresultsfound

--- client/src/components/NoResultsFound.js
+++ client/src/components/NoResultsFound.js
@@ -1,16 +1,25 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { UsersContext } from "./UsersContext";
+import { SearchContext } from "./SearchContext";
 import UserCard from "./UserCard";
 import styled from "styled-components";
 import Loading from "./Loaders/Loading";
 import NoResultsSvg from "../assets/NoResultsFoundSvg";
+import { useParams } from "react-router-dom";
 
 const NoResultsFound = () => {
   const { users, userStatus } = useContext(UsersContext);
+  const { query, setQuery } = useContext(SearchContext);
+  let { searchQuery } = useParams();
+
+  useEffect(() => {
+    setQuery(searchQuery);
+  });
+
   return (
     <div>
       <CenteredContent>
-        <h2>Oops... No Matching Results Found</h2>
+        <h2>Oops... No Matching Results Found For "{query}"</h2>
         <p>Try a new search or view recommended below</p>
         <NoResultsSvg />
         <h2>Recommended</h2>
